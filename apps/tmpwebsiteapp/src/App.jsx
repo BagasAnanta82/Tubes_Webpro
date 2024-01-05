ebf653bc5c3d850d979f33d4b55d9a9bd9838812
billyyuriaan <billy.yuriaan72@gmail.com>
2024-01-05 15:38:46 +0700

    [update] : add htaccess

--- apps/tmpwebsiteapp/src/App.jsx
+++ apps/tmpwebsiteapp/src/App.jsx
@@ -1,6 +1,6 @@
 import Dashboard from './views/Dashboard'
 import Login from './views/Login'
-import { HashRouter as Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
